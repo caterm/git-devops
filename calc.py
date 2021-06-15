3a1af7b28c0b7ede3515f11d35bcee22d979c4a5
Mark Cater <mark.cater1977@gmail.com>
2021-06-15 13:40:40 +0100

    Initial Commit of Calcualtor

---
+++ calc.py
@@ -0,0 +1,17 @@
+def calc(operation, first_number, second_number):
+    if operation == '+':
+        return first_number + second_number
+    elif operation == '-':
+        return first_number - second_number
+    elif operation == '*':
+        return first_number * second_number
+    elif operation == '/':
+        return first_number / second_number
+
+numberone = input('What is your first number?')
+numberone = int(numberone)
+numbertwo = input('What is your second number?')
+numbertwo = int(numbertwo)
+operator = input('What is your operation?')
+calculation_result = calc(operator, numberone, numbertwo)
+print(calculation_result)
